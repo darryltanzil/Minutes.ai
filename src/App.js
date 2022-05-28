9eb83055f7c2f48bb81b4c43dca7f36518eae3b6
darryltanzil <darryltanzil@gmail.com>
2022-05-28 16:00:26 -0700

    add artyom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,25 @@
 import './App.css';
+import Artyom from "artyom.js"
+
+
+const artyom = new Artyom();
+var settings = {
+    continuous:true, // Don't stop never because i have https connection
+    onResult:function(text){
+        console.log(text);
+    },
+    onStart:function(){
+        console.log("Dictation started by the user");
+    },
+    onEnd:function(){
+        alert("Dictation stopped by the user");
+    }
+};
+
+var UserDictation = artyom.newDictation(settings);
+
+// Start listening
+UserDictation.start();
 
 function App() {
   return (
