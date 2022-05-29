e708d8af7192214200a88c7654e3da7186335d09
Darryl Tanzil <darryltanzil@gmail.com>
2022-05-28 17:05:35 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,33 @@
-import React, { Component } from 'react';
 import './App.css';
+import Artyom from "artyom.js"
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { apiResponse: "" };
-  }
-  
-  callAPI() {
-    fetch("http://localhost:9000/testAPI")
-        .then(res => res.text())
-        .then(res => this.setState({ apiResponse: res }));
-  }
-  
-  componentWillMount() {
-    this.callAPI();
-  };
 
-  render() {
-    return(
+const artyom = new Artyom();
+var settings = {
+    continuous:true, // Don't stop never because i have https connection
+    onResult:function(text){
+        console.log(text);
+    },
+    onStart:function(){
+        console.log("Dictation started by the user");
+    },
+    onEnd:function(){
+        alert("Dictation stopped by the user");
+    }
+};
+
+var UserDictation = artyom.newDictation(settings);
+
+// Start listening
+UserDictation.start();
+
+function App() {
+  return (
     <div className="App">
       <header className="App-header">
-      <p className="App-intro">;{this.state.apiResponse}</p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -33,7 +39,6 @@ class App extends Component {
       </header>
     </div>
   );
-}
 }
 
 export default App;
